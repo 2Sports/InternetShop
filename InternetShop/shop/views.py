a20dfff56d58cb535e82fd356880f9fc8c89a69d
2Sports <hosnulline@gmail.com>
2024-06-23 17:20:10 +0300

    Добавил картинку на сайт

--- InternetShop/shop/views.py
+++ InternetShop/shop/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("HI! This is a internet shop!")
+    return HttpResponse("<h1>HI! This is a internet shop!</h1> <img src='https://s2.best-wallpaper.net/wallpaper/2560x1600/1907/Many-packs-of-US-dollars-money-currency_2560x1600.jpg'>")
