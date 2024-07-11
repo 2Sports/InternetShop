5a6aa89ba0548ee26f4a99027ac681391982cd4a
2Sports <hosnulline@gmail.com>
2024-07-11 16:27:25 +0300

    Почти добавил новую страницу(не работает)

--- InternetShop/shop/urls.py
+++ InternetShop/shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("product/<int:id>", views.view_product, name="view_product"),
 ]
+
 
